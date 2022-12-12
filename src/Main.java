e48a18ad80a48cc7ba9fe0d20a0b70aeb82dfe8e
Vasiliy-Gorbunov <79896130110@yandex.ru>
2022-12-12 14:36:16 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
 
+    public static void task1() {
+        System.out.println("Задание 1:");
+        int[] first = new int[3];
+        first[0] = 1;
+        first[1] = 2;
+        first[2] = 3;
+
+       /* for (int i = 0; i < 3; i++) {
+            System.out.println(first[i]);
+            }*/
+
+        double[] second = {1.57, 7.654, 9.986};
+
+        /*for (int i = 0; i < 3; i++) {
+            System.out.println(second[i]);
+            }*/
+
+        char[] third = {83, 75, 89};
+
+        /*for (int i = 0; i < 3; i++) {
+            System.out.print(third[i]);
+            }*/
     }
 }
+
